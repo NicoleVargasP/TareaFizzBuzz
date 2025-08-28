f8e73735f356f1dac530a50152b33d460c4dac21
Nicole Vargas <ariana.vargas.p@ucb.edu.bo>
2025-08-28 00:06:50 -0400

    refactor: Agregando detalles al index

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,13 @@
-import sumar from "./sumador";
+import {fizzbuzz } from "./fizzbuzz.js";  
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const first = document.querySelector("#numero-input");
+const form = document.querySelector("#fizzbuzz-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + fizzbuzz(firstNumber) + "</p>";
 });
